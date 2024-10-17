97167574ee2ffef9025f90eb631da449080689a0
R. Sam Craig <103201653+Samcraig77@users.noreply.github.com>
2024-10-16 17:46:13 -0700

    refactored code for readability
    The refactor breaks down the functions into smaller pieces, fetching each contributor from the array and spreading them into a map that uses the fetch functions to generate the elements

--- assets/data/contributors-component.js
+++ assets/data/contributors-component.js
@@ -1,47 +1,65 @@
 import { contributorsArray } from "./contributors.js"
 
-const contributorSection = document.getElementById("contributors-main-section")
+(function appendContributorsHtml() {
+    document.getElementById('contributors-main-section').innerHTML = `${[...renderContributorsHtml()].join('')}`
+})()
 
+//iterates through contributors arr and creates contributor card HTML for each adding them to an arr
+function renderContributorsHtml() {
+    let contributorsHtmlArr = []
 
-// Builds contributorMap from contributorsArray, making a card for each contributor
-const contributorMap = contributorsArray.map(contributor => 
-    `<article class="contributor-card">
+    contributorsHtmlArr = contributorsArray.map(contributor => {
+        const contributorAvatar = renderAvatarHtml(contributor.avatar, contributor.alt)
+        const contributorGitHub = renderGitHubHtml(contributor.gitHub)
+        const contributorLinkedIn = renderLinkedInHtml(contributor.linkedIn)
 
-        <h2>${contributor.name}</h2>
-        <br/>
-        <div class="img-container">
+        return `<article class="contributor-card">
 
-            <!-- Checks if contributors avatar exists, if not renders default picture -->
-            ${ contributor.avatar ? 
-            `<img class="contributor-avatar" src="${contributor.avatar}" alt="${contributor.alt}"/>`
-            : `<img class="contributor-avatar" src="/assets/images/contributors/placeholder-avatar.jpg" alt="placeholder avatar"/>`}
-                
-        </div>
-        <br/>
-        <h3>Find this contributor on:</h3>
-        <div class="social-links">
-                
-            <!-- Checks if gitHub link exists, only renders icon if present -->
-            ${ contributor.gitHub ? 
-            `<a href="${contributor.gitHub}" class="contributor-github" aria-label="github">
-            <i class="fa-brands fa-github social-icon fa-2x" aria-hidden="true" ></i></a>`
-            : '' }
-                
-            <!-- Checks if linkedIn link exists, only renders icon if present  -->
-            ${contributor.linkedIn ? `
-            <a href="${contributor.linkedIn}" aria-label="linked-in">
-            <i class="fa-brands fa-linkedin-in fa-2x social-icon" aria-hidden="true"></i>
-            </a>` 
-            : ''}
+                    <h2>${contributor.name}</h2>
+                    <br />
 
-        </div>
+                    <div class="avatar-container">
+                        ${contributorAvatar}
+                    </div>
 
-    </article>`
-).join('')
+                    <br />
 
+                    <h3>Find this contributor on:</h3>
+                    <span class="social-links">
+                        ${contributorGitHub}
+                        ${contributorLinkedIn}
+                    </span>
+                </article>
+            `
+    })
 
-function renderContributors() {
-    contributorSection.innerHTML = contributorMap
+    return contributorsHtmlArr
 }
 
-renderContributors()
+function renderAvatarHtml(avatarSrc, avatarAlt) {
+    if (avatarSrc) {
+        return `<img class="contributor-avatar" src="${avatarSrc}" alt="${avatarAlt}" />`
+    } else {
+        return `<img class="contributor-avatar" src="/assets/images/contributors/placeholder-avatar.jpg" alt="placeholder avatar" />`
+    }
+}
+
+function renderLinkedInHtml(linkedInUrl) {
+    if (linkedInUrl) {
+        return `<a href="${linkedInUrl}" aria-label="linked-in">
+                    <i class="fa-brands fa-linkedin-in fa-2x social-icon" aria-hidden="true"></i>
+                </a>`
+    } else {
+        return ''
+    }
+}
+
+function renderGitHubHtml(gitHubUrl) {
+    if (gitHubUrl) {
+        return `<a href="${gitHubUrl}" class="contributor-github" aria-label="github">
+                    <i class="fa-brands fa-github social-icon fa-2x" aria-hidden="true" ></i>
+                </a>`
+    } else {
+        return ''
+    }
+}
